591cfe80e384b9bdaf02b232c62d2351987a1bd6
Shubham Shrivas <60268544+shubham013333@users.noreply.github.com>
2025-08-06 21:39:59 +0530

    ok

--- ezblogs_backend/settings.py
+++ ezblogs_backend/settings.py
@@ -145,6 +145,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static'),
+#]
